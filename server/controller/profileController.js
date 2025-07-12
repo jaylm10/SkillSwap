5f54f6751a0c4b95520fe0cc3e2aaec2c99d4a1f
Jay <jay.parmar.lm10@gmail.com>
2025-07-12 18:38:56 +0530

    Add getAllProfile in controller

--- server/controller/profileController.js
+++ server/controller/profileController.js
@@ -62,3 +62,13 @@ exports.saveProfile = async (req, res) => {
     return res.status(500).json({ message: "Server error while saving profile" });
   }
 };
+
+exports.getAllProfiles = async(req,res)=>{
+    try {
+    const profiles = await UserProfile.find({ profileVisibility: 'Public' });
+    res.status(200).json(profiles);
+  } catch (error) {
+    console.error("Error fetching all profiles:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+}
