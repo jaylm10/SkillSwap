a34b30a4ac8f810fe84bf2fba4553880083f7e8e
Jay <jay.parmar.lm10@gmail.com>
2025-07-12 18:33:00 +0530

    Fetch all data from database

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
-import Header from '../components/Header'
-import ProfileCard from '../components/ProfileCard'
+import React, { useEffect, useState } from 'react';
+import Header from '../components/Header';
+import ProfileCard from '../components/ProfileCard';
 
 const Home = () => {
+  const [profiles, setProfiles] = useState([]);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/profiles");
+        if (!response.ok) throw new Error("Failed to load profiles");
+        const data = await response.json();
+        setProfiles(data);
+      } catch (err) {
+        console.log("Error loading profile", err);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
   return (
     <div>
-      <Header/>
-      <ProfileCard/>
+      <Header />
+      <div style={{ padding: '24px' }}>
+        {profiles.map((profile) => (
+          <ProfileCard
+            key={profile._id}
+            name={profile.name}
+            profilePhoto={profile.profilePhoto}
+            skillsOffered={profile.skillsOffered}
+            skillsWanted={profile.skillsWanted}
+            rating={profile.rating || "N/A"} // assuming you might add rating later
+            onRequestClick={() => alert(`Request sent to ${profile.name}`)}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
